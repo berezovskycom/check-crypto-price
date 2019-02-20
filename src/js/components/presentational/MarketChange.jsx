0d57120a3770618b58b84d8527b6dfe93dd9e632
vl <berezovsky@ex.ua>
2019-02-20 18:25:44 +0200

    new states for market change

--- src/js/components/presentational/MarketChange.jsx
+++ src/js/components/presentational/MarketChange.jsx
@@ -3,12 +3,19 @@ import PropTypes from 'prop-types';
 
 const MarketChange = ({ percent }) => (
   <div className="MarketChange">
-    <div className="MarketChange__Percent">{percent}<span className="secondaryColor">%</span></div>
+    <div className={`
+      MarketChange__Percent 
+      MarketChange__Percent${percent >= 0 ? `--up` : `--down`}
+    `}
+    >
+      {percent}
+      <span className="secondaryColor">%</span>
+    </div>
   </div>
 );
 
 MarketChange.propTypes = {
-  percent: PropTypes.number.isRequired,
+  percent: PropTypes.string.isRequired,
 };
 
 export default MarketChange;
