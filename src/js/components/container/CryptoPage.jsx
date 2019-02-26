8aa35b555d1e39868bea0d074eeb234ac13c746b
vl <berezovsky@ex.ua>
2019-02-26 21:56:09 +0200

    remove unnecessary content and make code cleaner

--- src/js/components/container/CryptoPage.jsx
+++ src/js/components/container/CryptoPage.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import Title from '../presentational/Title';
 import CurrentPrice from '../presentational/CurrentPrice';
 import MarketPrice from '../presentational/MarketPrice';
@@ -8,67 +9,54 @@ import MarketChange from '../presentational/MarketChange';
 import Rank from '../presentational/Rank';
 import Symbol from '../presentational/Symbol';
 import { parseCreatedAt, parseLastUpdated } from '../../misc/parseDate';
+import History from './History';
 
 class CryptoPage extends Component {
   constructor() {
     super();
     this.state = {
-      name: `Bitcoin`,
-      symbol: `btc`,
-      genesis_date: parseCreatedAt(`2009-01-03`),
-      rank: 1,
-      image: `https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579`,
-      current_price: `3584`,
-      high_24h: `3625.9`,
-      low_24h: `3589.9`,
-      market_cap_change_percentage_24h_in_currency: `-0.636`,
-      last_updated: `11:10`,
+      name: ``,
+      symbol: ``,
+      genesis_date: ``,
+      rank: 0,
+      current_price: ``,
+      high_24h: ``,
+      low_24h: ``,
+      market_cap_change_percentage_24h_in_currency: ``,
+      last_updated: ``,
       currency: `$`,
       links: [],
     };
   }
 
   componentDidMount() {
-    axios.get(`http://localhost:3001/api/${this.props.symbol}`)
+    const { id } = this.props;
+    axios.get(`http://localhost:3001/api/${id}`)
       .then((res) => {
         /* eslint camelcase: 0 */
         const { data } = res.data.dataOfCoin;
-        console.log(data);
         const {
           name,
           symbol,
           genesis_date,
           market_cap_rank,
-          image,
           market_data,
           last_updated,
           links,
         } = data;
+
         const {
           current_price,
           high_24h,
           low_24h,
-          market_cap_change_24h_in_currency,
           market_cap_change_percentage_24h_in_currency,
         } = market_data;
-        console.log(`
-          name = ${name}
-          symbol = ${symbol}
-          current_price = ${current_price.usd.toFixed(0)}$
-          high in 24h = ${high_24h.usd.toFixed(1)}$
-          low in 24h = ${low_24h.usd.toFixed(1)}$
-          date of birth = ${genesis_date}
-          rank = #${market_cap_rank}
-          last updated = ${last_updated}
-          imageURL = ${image.large}
-          change in 24h/% = ${market_cap_change_percentage_24h_in_currency.usd.toFixed(3)}%
-        `);
+
         this.setState({
           name,
           symbol,
           genesis_date: parseCreatedAt(genesis_date),
           rank: market_cap_rank,
-          image: image.large,
           current_price: `${current_price.usd.toFixed(0)}`,
           high_24h: `${high_24h.usd.toFixed(1)}`,
           low_24h: `${low_24h.usd.toFixed(1)}`,
@@ -78,9 +66,6 @@ class CryptoPage extends Component {
           links,
         });
       });
-
-    axios.post(`http://localhost:3001/api/history`, {date: '30-12-2017'})
-      .then(res => console.log(`old bitcoin: `, res));
   }
 
   render() {
@@ -89,7 +74,6 @@ class CryptoPage extends Component {
       symbol,
       genesis_date,
       rank,
-      image,
       current_price,
       high_24h,
       low_24h,
@@ -112,9 +96,9 @@ class CryptoPage extends Component {
       <div className="CryptoPage">
         <section className="Theme ThemeHeader ThemeHeader--dark">
           <div className="ThemeHeader__DarkCover" />
-          <video className="ThemeHeader__Video" muted autoPlay loop>
+          {/* <video className="ThemeHeader__Video" muted autoPlay loop>
             <source src="./assets/video/blackpepper.mp4" type="video/mp4" />
-          </video>
+          </video> */}
           <div className="CryptoPage__Header Header">
             <Rank value={rank} className="Header__Top Header__Rank" />
             <Symbol value={symbol} />
@@ -140,12 +124,21 @@ class CryptoPage extends Component {
                 </li>
               ))
             }
-            <Link to="/menu" className="BackTo__Menu">Back to menu</Link>
+            <li>
+              <History of={name.toLowerCase()} />
+            </li>
+            <li>
+              <Link to="/menu" className="BackTo__Menu">Back to menu</Link>
+            </li>
           </ul>
         </section>
       </div>
     );
   }
 }
+
+CryptoPage.propTypes = {
+  id: PropTypes.string.isRequired,
+};
 
 export default CryptoPage;
