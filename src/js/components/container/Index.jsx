2a9e6d261b80439d04bf82dee3ef417ff0e0d8f0
vl <berezovsky@ex.ua>
2019-02-26 21:57:03 +0200

    new page

--- src/js/components/container/Index.jsx
+++ src/js/components/container/Index.jsx
@@ -1,16 +1,9 @@
 import React from 'react';
-import { Hashrouter as Router } from "react-router-dom";
-import CryptoPage from './CryptoPage';
-
-function handleHash() {
-  const { hash } = window.location;
-  return hash.slice(-3);
-}
 
 const Index = () => (
-  <Router>
-    <CryptoPage symbol={handleHash()} />
-  </Router>
+  <div>
+    <h1>Welcome to crypto page.</h1>
+  </div>
 );
 
 export default Index;
